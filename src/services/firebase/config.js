87ab76db97a43d3b2fce2b8cb15e53af905aebee
Atul Bipin <atulbipin@gmail.com>
2019-02-01 19:56:03 -0800

    Fix config.js

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,13 +1,4 @@
-// const config = {
-//     apiKey: "AIzaSyBgTmIIuNpnUWldNGzl_FkMOMgn_JiwqF4",
-//     authDomain: "ezplanner-db.firebaseapp.com",
-//     databaseURL: "https://ezplanner-db.firebaseio.com",
-//     projectId: "ezplanner-db",
-//     storageBucket: "ezplanner-db.appspot.com",
-//     messagingSenderId: "458952217432"
-//   };
-
-const config = {
+export default {
   apiKey: 'AIzaSyCvE7XYUN1iA5z5udNXp0Kng1U5BE_4Qk0',
   authDomain: 'uwezplanner.firebaseapp.com',
   databaseURL: 'https://uwezplanner.firebaseio.com',
@@ -15,5 +6,3 @@ const config = {
   storageBucket: 'uwezplanner.appspot.com',
   messagingSenderId: '78485427704'
 };
-
-export default config;
